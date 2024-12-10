a974e524a345139f84ad90dbc717704904043821
Jyothsna Balla <2201600176mca@gmail.com>
2024-12-10 14:38:05 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BirthChart from './components/BirthChart';
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<BirthChart />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ server/models/numerologyModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const NumerologySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  dob: { type: String, required: true },
+  lifePathNumber: { type: Number, required: true },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Numerology", NumerologySchema);
